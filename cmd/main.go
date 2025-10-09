6c51b946f8e143bd6eb3dcdaf7deeb32c45a9812
Wladim1r <Volodoy53728@mail.ru>
2025-10-09 23:27:04 +0300

    parce and check on original for one product from WB

--- cmd/main.go
+++ cmd/main.go
@@ -5,13 +5,23 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/WWoi/web-parcer/internal/handlers"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 
 	r := gin.Default()
+
+	r.GET("/art/:articul", handlers.SearchByArticul)
 
 	server := http.Server{
 		Addr:    os.Getenv("SVR_PORT"),
