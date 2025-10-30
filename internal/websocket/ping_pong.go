486b975231a75ba347f0df8ffcbe4d87e9305ce7
Tonic56 <qtonic56@gmail.com>
2025-10-30 21:33:00 +0300

    improved ping pong

--- internal/websocket/ping_pong.go
+++ internal/websocket/ping_pong.go
@@ -1,22 +1,80 @@
 package websocket
 
 import (
-	"time"
+	"context"
 	"log/slog"
+	"time"
+
 	"github.com/gorilla/websocket"
 )
 
-func (c *WSclient) setupPingPong() {
+func (c *WSclient) setupPingPong(ctx context.Context) {
+
 	c.conn.SetPingHandler(func(appData string) error {
+
 		slog.Info("Ping from Binance, answer pong")
 
 		// Отправляем pong обратно
+
 		err := c.conn.WriteControl(
-			websocket.PongMessage,         // Тип: PONG
-			[]byte(appData),               // Тот же payload
+
+			websocket.PongMessage, // Тип: PONG
+
+			[]byte(appData), // Тот же payload
+
 			time.Now().Add(1*time.Second), // Deadline
+
 		)
 
-		return err
+		if err != nil {
+
+			slog.Error("Failed to send pong", "error", err)
+
+			return err
+
+		}
+
+		return nil
+
 	})
+
+	// health check
+
+	ticker := time.NewTicker(3 * time.Second)
+
+	defer ticker.Stop()
+
+	for {
+
+		select {
+
+		case <-ticker.C:
+
+			slog.Debug("Sending ping to Binance")
+
+			if err := c.conn.WriteControl(
+
+				websocket.PingMessage,
+
+				[]byte{},
+
+				time.Now().Add(5*time.Second),
+			); err != nil {
+
+				slog.Warn("Failed to send ping", "error", err)
+
+				return // выход из горутины
+
+			}
+
+		case <-ctx.Done():
+
+			slog.Info("Ping-pong handler stopped")
+
+			return
+
+		}
+
+	}
+
 }
